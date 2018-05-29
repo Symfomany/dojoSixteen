810924db982954dd3989e07811a0fb26a343051e
Symfomany <zuzu38080@gmail.com>
2018-05-29 11:13:30 +0200

    add changes

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
 function arrayLeaders(tableau = []) {
-  // you code here
-  // copie du tableau initial
-  let tabAccumul = tableau;
-  // ANDO
-  //   const tab = tableau.forEach((element, index) => {
-  //     // supprime le premier élément du tableau
-  //     tabAccumul = tabAccumul.shift();
-  //     // reduce pour renvoyer la somme des éléments du tableau
-  //     tabAccumul = tableau.reduce((accumulateur, valcourante, index, array) => {
-  //       return accumulateur + valcourante;
-  //     }, 0);
-  //     console.log(tabAccumul);
-  //   });
-  //   return tab;
-  let clone = [...tableau];
-  let tabSommes = [];
-  let test = tableau.map(elt => {
-    clone.shift();
-    tabSommes.push(clone.reduce((x, y) => x + y, 0));
+  let gitClown = [],
+    tabBis = [];
+  tableau.map((elt, index, array) => {
+    tabBis.push(array.slice(index + 1).reduce((a, b) => a + b, 0));
+    tabBis[index] < tableau[index] ? gitClown.push(tableau[index]) : false;
   });
-  const tabLeader = [];
-  tabSommes.forEach((elt, index) => {
-    if (elt < tableau[index]) {
-      tabLeader.push(tableau[index]);
-    }
-  });
-  return tabLeader;
+  return gitClown;
 }
 
 console.log(arrayLeaders([20, 1, 2, 3, 4, 0]));
